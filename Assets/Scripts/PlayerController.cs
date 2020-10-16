b31b523e8bbcf0b74e9c849dc3a7c5cf81449f23
Benjamin Nordin <sockless@protonmail.com>
2020-10-16 22:34:57 +0200

    Add Basic Movement
    Added the basicest movement you can get using the new unity input system, I still don't know how to use it properly, but this is fun nonetheless

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D m_rigidbody;
+    Vector2 m_movement;
+    [SerializeField]float m_movespeed;
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+    public void onMove(InputAction.CallbackContext context)
+    {
+        m_movement = context.ReadValue<Vector2>();
+    }
+
+
+    public void Move(Vector2 movement)
+    {
+
+        m_rigidbody.velocity = new Vector2(movement.x, m_rigidbody.velocity.y);
+    }
     void Update()
     {
-        
+        Move(m_movement * m_movespeed);
     }
 }
